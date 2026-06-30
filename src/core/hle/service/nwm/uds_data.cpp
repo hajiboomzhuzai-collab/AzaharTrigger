80b476e169137d1d2ac44fa230d8ca6ca6f4e6aa
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-01 05:00:00 +0800

    Remove connection_type assignment in GenerateEAPoLStartFrame
    Removed assignment of connection_type from EAPoLStartPacket.

--- src/core/hle/service/nwm/uds_data.cpp
+++ src/core/hle/service/nwm/uds_data.cpp
@@ -289,7 +289,6 @@ SecureDataHeader ParseSecureDataHeader(std::span<const u8> data) {
 std::vector<u8> GenerateEAPoLStartFrame(u16 association_id, const NodeInfo& node_info) {
     EAPoLStartPacket eapol_start{};
     eapol_start.association_id = association_id;
-    eapol_start.connection_type = conn_type;
     eapol_start.node.friend_code_seed = node_info.friend_code_seed;
 
     std::copy(node_info.username.begin(), node_info.username.end(),
