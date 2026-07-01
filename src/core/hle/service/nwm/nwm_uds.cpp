f17e7574c6d58d111b2fbb39c801e81cb0d0a3ef
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-02 06:04:12 +0800

    Fix node deserialization in EAPoL start packet

--- src/core/hle/service/nwm/nwm_uds.cpp
+++ src/core/hle/service/nwm/nwm_uds.cpp
@@ -225,8 +225,8 @@ void NWM_UDS::HandleEAPoLPacket(const Network::WifiPacket& packet) {
 
         auto eapol_start = ParseCompatibleEAPoLStart(packet.data);
 
-        auto node = DeserializeNodeInfo(eapol_start.node);
-
+        auto node = DeserializeNodeInfo(eapol_start.packet.node);
+        
         if (eapol_start.packet.connection_type == ConnectionType::Client) {
             // Get an unused network node id
             u16 node_id = GetNextAvailableNodeId();
