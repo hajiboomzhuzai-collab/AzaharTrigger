5ba01514d4c215f1f8e02b3c4855da51df831dfc
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-12 21:47:25 +0800

    Update nwm_uds.cpp

--- src/core/hle/service/nwm/nwm_uds.cpp
+++ src/core/hle/service/nwm/nwm_uds.cpp
@@ -609,12 +609,6 @@ node_info.resize(connection_status.max_nodes);;
 }
 
 NWM_UDS::Node* NWM_UDS::FindNodeByNodeId(u16 node_id) {
-    LOG_ERROR(Service_NWM,
-              "FindNodeByNodeId ENTER id={} status={} node_map={}",
-              node_id,
-              static_cast<u32>(connection_status.status),
-              node_map.size());
-
     if (node_id == 0 || node_id > UDSMaxNodes) {
         LOG_ERROR(Service_NWM,
                   "FindNodeByNodeId invalid id={}",
@@ -629,63 +623,21 @@ NWM_UDS::Node* NWM_UDS::FindNodeByNodeId(u16 node_id) {
                   static_cast<u32>(connection_status.status),
                   node_map.size());
 
-        for (u16 i = 1; i <= UDSMaxNodes; i++) {
-            if (node_lookup[i]) {
-                const auto& mac = *node_lookup[i];
-                LOG_ERROR(Service_NWM,
-                          "  lookup[{}] = {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-                          i,
-                          mac[0], mac[1], mac[2],
-                          mac[3], mac[4], mac[5]);
-            } else {
-                LOG_ERROR(Service_NWM,
-                          "  lookup[{}] = <empty>",
-                          i);
-            }
-        }
-
         return nullptr;
     }
 
     const auto& lookup_mac = *node_lookup[node_id];
 
-    LOG_ERROR(Service_NWM,
-              "FindNodeByNodeId lookup hit id={} mac={:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-              node_id,
-              lookup_mac[0], lookup_mac[1], lookup_mac[2],
-              lookup_mac[3], lookup_mac[4], lookup_mac[5]);
-
     auto it = node_map.find(lookup_mac);
 
     if (it == node_map.end()) {
         LOG_ERROR(Service_NWM,
-                  "FindNodeByNodeId map missing id={} mac={:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X} node_map_size={}",
+                  "FindNodeByNodeId map missing id={} node_map_size={}",
                   node_id,
-                  lookup_mac[0], lookup_mac[1], lookup_mac[2],
-                  lookup_mac[3], lookup_mac[4], lookup_mac[5],
                   node_map.size());
-
-        for (const auto& [map_mac, node] : node_map) {
-            LOG_ERROR(Service_NWM,
-                      "  map: id={} connected={} spec={} reconnecting={} mac={:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-                      node.node_id,
-                      node.connected,
-                      node.spec,
-                      node.reconnecting,
-                      map_mac[0], map_mac[1], map_mac[2],
-                      map_mac[3], map_mac[4], map_mac[5]);
-        }
 
         return nullptr;
     }
-
-    LOG_ERROR(Service_NWM,
-              "FindNodeByNodeId SUCCESS id={} connected={} spec={} reconnecting={} node_id={}",
-              node_id,
-              it->second.connected,
-              it->second.spec,
-              it->second.reconnecting,
-              it->second.node_id);
 
     return &it->second;
 }
