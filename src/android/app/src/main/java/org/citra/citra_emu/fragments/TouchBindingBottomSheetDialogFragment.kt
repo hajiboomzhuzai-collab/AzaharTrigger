0b3116a016731b0994915d3fc34e3a22b82de4b7
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-17 00:51:48 +0800

    Create TouchBindingBottomSheetDialogFragment.kt

---
+++ src/android/app/src/main/java/org/citra/citra_emu/fragments/TouchBindingBottomSheetDialogFragment.kt
@@ -0,0 +1,53 @@
+package org.citra.citra_emu.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.citra.citra_emu.databinding.DialogInputBindingBinding
+
+class TouchBindingBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: DialogInputBindingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding =
+            DialogInputBindingBinding.inflate(
+                inflater,
+                container,
+                false
+            )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textTitle.text = "Bind Touch Point"
+
+        binding.textMessage.text =
+            "Press any physical controller button."
+
+        binding.buttonClear.visibility = View.GONE
+
+        binding.buttonCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
