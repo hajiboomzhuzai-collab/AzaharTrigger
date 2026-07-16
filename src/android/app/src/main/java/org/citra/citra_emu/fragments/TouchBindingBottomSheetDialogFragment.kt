891cfcde60e0824fa4dabb210df03fdc1d2bf991
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-17 01:10:01 +0800

    Update TouchBindingBottomSheetDialogFragment.kt

--- src/android/app/src/main/java/org/citra/citra_emu/fragments/TouchBindingBottomSheetDialogFragment.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/fragments/TouchBindingBottomSheetDialogFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import org.citra.citra_emu.databinding.DialogInputBindingBinding
+import org.citra.citra_emu.databinding.DialogInputBinding
 
 class TouchBindingBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
