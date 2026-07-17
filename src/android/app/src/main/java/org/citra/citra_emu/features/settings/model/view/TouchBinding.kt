ef95bc48e860de8d36404df6cbd25e00a9a7a51a
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-17 14:07:24 +0800

    Update TouchBinding.kt

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/view/TouchBinding.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/view/TouchBinding.kt
@@ -3,6 +3,7 @@ package org.citra.citra_emu.features.settings.model.view
 data class TouchBinding(
     val keyCode: Int,
     val axis: Int = -1,
+    val positive: Boolean = true,
     val x: Float,
     val y: Float
 )
