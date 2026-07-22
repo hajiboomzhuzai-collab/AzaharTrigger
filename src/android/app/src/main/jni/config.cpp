97347a28381eea8658695b1b473382baf76e190a
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-23 04:29:14 +0800

    Update config.cpp

--- src/android/app/src/main/jni/config.cpp
+++ src/android/app/src/main/jni/config.cpp
@@ -329,7 +329,10 @@ void Config::ReadValues() {
 
     // Web Service
     NetSettings::values.web_api_url =
-        android_config->GetString("WebService", "web_api_url", "https://api.citra-emu.org");
+        android_config->GetString("WebService", "web_api_url", "http://88.198.47.46:5000");
+    if (NetSettings::values.web_api_url == "https://api.citra-emu.org") {
+        NetSettings::values.web_api_url = "http://88.198.47.46:5000";
+    }
     NetSettings::values.citra_username =
         android_config->GetString("WebService", "citra_username", "");
     NetSettings::values.citra_token = android_config->GetString("WebService", "citra_token", "");
