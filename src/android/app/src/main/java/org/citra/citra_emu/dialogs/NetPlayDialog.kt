daa49f98d1965085ef0c2924220303958efecbc2
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-23 18:09:19 +0800

    Update NetPlayDialog.kt

--- src/android/app/src/main/java/org/citra/citra_emu/dialogs/NetPlayDialog.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/dialogs/NetPlayDialog.kt
@@ -176,6 +176,7 @@ class NetPlayDialog(context: Context) : BottomSheetDialog(context) {
         val name: String,
         val type: Int,
         val id: Int = 0
+        val subtitle: String = ""
     ) {
         companion object {
             const val MULTIPLAYER_ROOM_TEXT = 1
@@ -266,8 +267,17 @@ class NetPlayDialog(context: Context) : BottomSheetDialog(context) {
                 netPlayItems.add(NetPlayItems(NetPlayItems.MULTIPLAYER_ROOM_TEXT, roomInfo[0], NetPlayItems.TYPE_TEXT))
                 netPlayItems.add(NetPlayItems(NetPlayItems.MULTIPLAYER_ROOM_COUNT, "${infos.size - 1}/${roomInfo[1]}", NetPlayItems.TYPE_TEXT))
                 netPlayItems.add(NetPlayItems(NetPlayItems.MULTIPLAYER_SEPARATOR, "", NetPlayItems.TYPE_SEPARATOR))
+        
                 for (i in 1 until infos.size) {
-                    netPlayItems.add(NetPlayItems(NetPlayItems.MULTIPLAYER_ROOM_MEMBER, infos[i], NetPlayItems.TYPE_BUTTON))
+                    val parts = infos[i].split("|")
+                    netPlayItems.add(
+                        NetPlayItems(
+                            NetPlayItems.MULTIPLAYER_ROOM_MEMBER,
+                            parts.getOrElse(0) { "" },  // nickname
+                            NetPlayItems.TYPE_BUTTON,
+                            subtitle = parts.getOrElse(2) { "" }  // game_name
+                        )
+                    )
                 }
             }
         }
