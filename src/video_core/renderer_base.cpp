34ec28ccd9eaef1926b79b92979b258e38004b9c
hajiboomzhuzai-collab <hajiboomzhuzai@gmail.com>
2026-07-24 05:54:40 +0800

    Update renderer_base.cpp

--- src/video_core/renderer_base.cpp
+++ src/video_core/renderer_base.cpp
@@ -17,16 +17,19 @@ RendererBase::RendererBase(Core::System& system_, Frontend::EmuWindow& window,
 
 RendererBase::~RendererBase() = default;
 
-u32 RendererBase::GetResolutionScaleFactor() {
+u32 RendererBase::GetResolutionScaleFactor(bool is_top_screen) {
     const auto graphics_api = Settings::values.graphics_api.GetValue();
     if (graphics_api == Settings::GraphicsAPI::Software) {
         // Software renderer always render at native resolution
         return 1;
     }
 
-    const u32 scale_factor = Settings::values.resolution_factor.GetValue();
-    return scale_factor != 0 ? scale_factor
-                             : render_window.GetFramebufferLayout().GetScalingRatio();
+    // TEST: separate screen scaling
+    if (is_top_screen) {
+        return 2; // Top screen = 2x
+    } else {
+        return 1; // Bottom screen = 1x
+    }
 }
 
 void RendererBase::UpdateCurrentFramebufferLayout(bool is_portrait_mode) {
